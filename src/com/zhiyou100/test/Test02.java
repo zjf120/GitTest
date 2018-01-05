adffdf94330987b12efac2cbff8789ae9ff026f1
zjf120 <353459028@qq.com>
2018-01-05 21:57:41 +0800

    新建文件

---
+++ src/com/zhiyou100/test/Test02.java
@@ -0,0 +1,17 @@
+package com.zhiyou100.test;
+
+/**  
+* @ClassName: Test02  
+* @Description: TODO
+* @author zjf  
+* @date 2018年1月5日  下午9:55:48
+*  
+*/
+
+public class Test02 {
+	private void mian() {
+		// TODO Auto-generated method stub
+		System.out.println("新建文件test02");
+	}
+
+}
